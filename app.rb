c4eb6855d0e04507fc14bfd342ffeb8367e48b6a
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-10 19:04:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
     @num_1 = params[:num1]
     @num_2 = params[:num2]
     # erb :multiply
-    @num_1*@num_2
-    # "#{@num_1*@num_2}"
+    # @num_1*@num_2
+    "#{@num_1*@num_2}"
   end
 end
